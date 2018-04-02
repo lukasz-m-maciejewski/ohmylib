617ff14a9abbaf03efad183064d5ce2f6e79c8db
Lukasz Maciejewski <lukasz.m.maciejewski@gmail.com>
2018-04-02 17:55:46 +0200

    up to find
    * added naive implementation of find
    * added implementation and tests for compare

--- string_view.hpp
+++ string_view.hpp
@@ -1,7 +1,7 @@
 #ifndef MY_STRING_VIEW
 #define MY_STRING_VIEW
 
-
+#include <algorithm>
 #include <iterator>
 #include <limits>
 #include <string>
@@ -26,18 +26,20 @@ public:
     using size_type = std::size_t;
     using difference_type = std::ptrdiff_t;
 
+    static constexpr size_type npos = size_type(-1);
+
     static_assert(std::is_same<CharT, typename Traits::char_type>::value, "char traits need to refer to base type");
 
-    constexpr basic_string_view()
+    constexpr basic_string_view() noexcept
         : data_ptr{nullptr}, sz{0u}
     {
     }
 
-    constexpr basic_string_view(const basic_string_view& ) = default;
-    constexpr basic_string_view& operator=(const basic_string_view& ) = default;
+    constexpr basic_string_view(const basic_string_view& ) noexcept = default;
+    constexpr basic_string_view& operator=(const basic_string_view& ) noexcept = default;
 
     constexpr basic_string_view(const_pointer c)
-        : data_ptr{c}, sz{traits_type::length(c)}
+        : data_ptr{c}, sz{c == nullptr ? 0ull : traits_type::length(c)}
     {
     }
 
@@ -78,9 +80,89 @@ public:
     constexpr size_type max_size() const { return std::numeric_limits<size_type>::max(); }
     constexpr bool empty() const { return (sz == 0ull); }
 
+    constexpr void remove_prefix(size_type n) { data_ptr += n; }
+    constexpr void remove_suffix(size_type n) { sz -= n; }
 
+    constexpr void swap(basic_string_view& other)
+    {
+        std::swap(other.data_ptr, data_ptr);
+        std::swap(other.sz, sz);
+    }
+
+    size_type copy(CharT* dest, size_type count, size_type pos = 0ull) const
+    {
+        if (pos >= sz)
+            throw std::out_of_range(__PRETTY_FUNCTION__);
+
+        auto rcount = std::min(count, sz - pos);
+        auto dest_end_it = std::copy_n(data_ptr, rcount, dest);
+        return std::distance(dest, dest_end_it);
+    }
+
+    basic_string_view substr(size_type pos = 0, size_type count = npos) const
+    {
+        if (pos >= sz)
+            throw std::out_of_range(__PRETTY_FUNCTION__);
+
+        return basic_string_view(data_ptr + pos, std::min(count, sz - pos));
+    }
+
+    constexpr int compare(basic_string_view v) const
+    {
+        return compare(0, npos, v, 0, npos);
+    }
+
+    constexpr int compare(size_type pos, size_type count, basic_string_view v) const
+    {
+        return compare(pos, count, v, 0, npos);
+    }
+
+    constexpr int compare(size_type pos1, size_type count1, basic_string_view v,
+                          size_type pos2, size_type count2) const
+    {
+        if (pos1 >= sz or pos2 >= v.sz)
+            throw std::out_of_range(__PRETTY_FUNCTION__);
+
+        auto rlen = std::min(std::min(sz - pos1, count1), std::min(v.sz - pos2, count2));
+        auto traits_rv = traits_type::compare(data_ptr + pos1, v.data_ptr + pos2, rlen);
+
+        if (traits_rv != 0)
+            return traits_rv;
+        else
+            return sz < v.sz ? -1 : (sz > v.sz ? 1 : 0);
+    }
+
+    constexpr int compare(const CharT* s) const
+    {
+        return compare(basic_string_view(s));
+    }
+
+    constexpr int compare(size_type pos, size_type count, const CharT* s) const
+    {
+        return substr(pos, count).compare(basic_string_view(s));
+    }
+
+    constexpr int compare(size_type pos1, size_type count1,
+                          const CharT* s, size_type count2)
+    {
+        return substr(pos1, count1).compare(basic_string_view(s, count2));
+    }
+
+    constexpr size_type find(basic_string_view v, size_type pos = 0) const
+    {
+        auto l = v.length();
+        for (auto t_pos = size_type{0}; t_pos - l < sz; ++t_pos)
+        {
+            int res = substr(t_pos, l).compare(v);
+            if (res == 0)
+                return t_pos;
+        }
+
+        return npos;
+    }
 
 private:
+
     const_pointer data_ptr;
     size_type sz;
 };
--- string_view.test.cpp
+++ string_view.test.cpp
@@ -11,6 +11,7 @@ TEST_CASE("string_view construction")
         REQUIRE(sv.data() == nullptr);
         REQUIRE(sv.size() == 0u);
         REQUIRE(sv.length() == 0u);
+        REQUIRE(sv.empty() == true);
     }
 
     SECTION("view constructed from literal provides proper length")
@@ -21,7 +22,7 @@ TEST_CASE("string_view construction")
         REQUIRE(sv.data() == str);
         REQUIRE(sv.size() == 3ull);
         REQUIRE(sv.length() == 3ull);
-
+        REQUIRE(sv.empty() == false);
     }
 
     SECTION("view constructed with explicit length ignores actual lenght of the string")
@@ -32,6 +33,7 @@ TEST_CASE("string_view construction")
         REQUIRE(sv.data() == str);
         REQUIRE(sv.size() == 20ull);
         REQUIRE(sv.length() == 20ull);
+        REQUIRE(sv.empty() == false);
     }
 }
 
@@ -98,5 +100,86 @@ TEST_CASE("begin and end variants")
         {
             condition_satisfied_acc = condition_satisfied_acc && (*it == *st_rev);
         }
+
+        REQUIRE(condition_satisfied_acc);
+    }
+}
+
+TEST_CASE("will allow read only access to elements")
+{
+    my::string_view sv("abcdefghijklmnopqrstuwxyz");
+    SECTION("will grant unchecked access via operator[]")
+    {
+        REQUIRE(sv[10] == 'k');
+        char out_of_bounds{};
+        REQUIRE_NOTHROW(out_of_bounds = sv[123]);
+
+        (void)out_of_bounds;
+    }
+
+    SECTION("will grant bounds-checked access via at function")
+    {
+        REQUIRE(sv.at(15) == 'p');
+        char out_of_bounds{};
+        REQUIRE_THROWS_AS(out_of_bounds = sv.at(123), std::out_of_range);
+
+        (void)out_of_bounds;
+    }
+
+    SECTION("will grant access to first and last element by dedicated functions")
+    {
+        REQUIRE(sv.front() == 'a');
+        REQUIRE(sv.back() == 'z');
+    }
+}
+
+TEST_CASE("will allow comparing views")
+{
+    SECTION("when comparing strings of equal length comparison is lexicographic")
+    {
+        my::string_view sv1{"aaa"};
+        my::string_view sv2("aba");
+        auto result_1_2 = sv1.compare(sv2);
+        auto result_2_1 = sv2.compare(sv1);
+        REQUIRE(result_1_2 < 0);
+        REQUIRE(result_2_1 > 0);
+    }
+
+    SECTION("when comparing strings of different length with same common prefix then longer one is greater")
+    {
+        my::string_view sv1{"aaa"};
+        my::string_view sv2{"aaaa"};
+
+        auto result_1_2 = sv1.compare(sv2);
+        auto result_2_1 = sv2.compare(sv1);
+
+        REQUIRE(result_1_2 < 0);
+        REQUIRE(result_2_1 > 0);
+    }
+
+    SECTION("when comparing strings of different length with different prefixes then prefix determines comparison")
+    {
+        my::string_view sv1{"aaa"};
+        my::string_view sv2{"abab"};
+
+        auto result_1_2 = sv1.compare(sv2);
+        auto result_2_1 = sv2.compare(sv1);
+
+        REQUIRE(result_1_2 < 0);
+        REQUIRE(result_2_1 > 0);
+    }
+
+    SECTION("string view compares with c-string")
+    {
+        my::string_view sv {"aaaa"};
+        auto cstr = std::make_unique<char[]>(5);
+        cstr.get()[0] = 'a';
+        cstr.get()[1] = 'a';
+        cstr.get()[2] = 'a';
+        cstr.get()[3] = 'a';
+        cstr.get()[4] = 0;
+
+        auto result = sv.compare(cstr.get());
+        REQUIRE(result == 0);
     }
 }
